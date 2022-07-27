4a6c45d82f82f10067f1da9a93b823d8370053b3
mysticinferno <110055773+mysticinferno@users.noreply.github.com>
2022-07-27 18:34:24 +0530

    Update calculator.c
    added division function

--- calculator.c
+++ calculator.c
@@ -14,3 +14,21 @@ void multiplication(long int a,long int b)
 {
     printf("%ld", a * b);
 }
+//division Function
+void division(long int a, long int b)
+{
+  if(b!=0){
+      if (a % b != 0)
+      {
+        float c = a / (float)b;
+        printf("%f", c);
+      }
+      else
+      {
+        printf("%ld", a / b);
+      }
+    }
+  else{
+    printf("division by 0 ERROR");
+  }
+}
